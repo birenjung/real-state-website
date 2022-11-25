55411da505646fb7945532e46f9f85d3d89f792d
Birendra Jung Rai <bjsampang@gmail.com>
2022-11-25 19:15:23 +0545

    second commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-let burgerMenu = document.querySelector(".burger-menu");
-let menuLinks = document.querySelector(".menu-links");
+const burgerMenu = document.querySelector(".burger-menu");
+const menuLinks = document.querySelector(".menu-links");
+const menu = document.querySelector(".menu");
+
 
 function bgMenu() {
     burgerMenu.classList.toggle("is-active");
     menuLinks.classList.toggle("is-active");
 }
+
+
